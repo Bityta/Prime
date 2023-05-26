f6f950654da527197bc40b31cd00810a14306e08
Bityta <108558654+Bityta@users.noreply.github.com>
2023-05-27 01:09:50 +0300

    firster version

---
+++ Prime.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Prime {
+
+    public static boolean prime(int num){
+
+        for (int i = 2; i < Math.sqrt(num) + 1; i++){
+            if (num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static ArrayList<Integer> primeNumber(int num){
+
+        ArrayList<Integer> array = new ArrayList<>();
+
+        for (int i = 1; i < Math.sqrt(num)+1; i++){
+            if (num % i == 0){
+                array.add(i);
+                if (i != num / i){
+                    array.add(num / i);
+                }
+            }
+        }
+
+        return array;
+
+    }
+}
